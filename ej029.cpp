ddaa599e2a17e81c0159099c640df7bcd5253b81
davisitomendozita011xd <dmendoza@alumno.montepiedra.edu.ec>
2025-10-16 14:32:16 -0500

    Add files via upload

---
+++ ej029.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Ejercicio 29
+// Descripción: (ver documento principal para detalles)
+// Tema: Algoritmos avanzados / STL / Grafos / DP
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // Escribe tu solución aquí
+    
+    return 0;
+}
