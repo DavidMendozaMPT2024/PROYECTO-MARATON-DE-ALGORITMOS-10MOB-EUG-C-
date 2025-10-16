44bbeb0d6a5ebe961a14a8342f4bc67a6564906d
davisitomendozita011xd <dmendoza@alumno.montepiedra.edu.ec>
2025-10-16 14:35:13 -0500

    Add files via upload

---
+++ ej163.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Ejercicio 163
+// Título: (ver documento '100 Ejercicios C++ Ultra Avanzados')
+// Enunciado breve: Implementación avanzada relacionada con el ejercicio 163.
+// Restricciones: Optimizar tiempo/memoria cuando aplique.
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Escribe tu solución aquí
+
+    return 0;
+}
